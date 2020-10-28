1b178ef7b5bb7924bee411eb57618f11072e7a9e
Kevin Thorne <kevinpthorne@gmail.com>
2020-10-27 23:56:44 -0600

    Testing needed. No change requests added yet

--- lib/layer/security.py
+++ lib/layer/security.py
@@ -1,12 +1,14 @@
 """
 Roles and Security Layer
 """
+import dictdiffer
+
 from lib.layer import Layer
 
 CAN_CREATE = 'can_create'  # falsy. Base rule is to reject
 CANT_READ = 'cant_read'  # truthy. Base rule is to allow. Deletes fields during read
 CAN_UPDATE = 'can_update'  # falsy. Base rule is to reject
-CAN_DELETE = 'can_delete'  # falsy. Base rule is to reject
+CAN_DESTROY = 'can_delete'  # falsy. Base rule is to reject
 CAN_APPROVE = 'can_approve'  # falsy. Base rule is to reject
 
 ROLES = {
@@ -14,13 +16,13 @@ ROLES = {
         CAN_CREATE: [
             "*"
         ],
-        CAN_UPDATE: {  # falsy. Base rule is to reject
+        CAN_UPDATE: {
             "*"
         },
-        CAN_DELETE: [  # falsy. Base rule is to reject
+        CAN_DESTROY: [
             "*"
         ],
-        CAN_APPROVE: {  # falsy. Base rule is to reject
+        CAN_APPROVE: {
             "*"
         }
     },
@@ -77,30 +79,112 @@ class SecurityLayer(Layer):
 
     @staticmethod
     def _get_model_name_from_repo_cls(repo_cls):
-        return repo_cls.__class__.__name__.lower().replace('repository', '')
+        return repo_cls.__name__.lower()
 
     def on_create(self, repo_cls, new_model):
         model_name = self._get_model_name_from_repo_cls(repo_cls)
+
+        if CAN_CREATE not in self.user_role:
+            raise SecurityException(f'Creating {model_name} records is not allowed')
+
+        if '*' in self.user_role[CAN_CREATE]:
+            return
+
         if model_name not in self.user_role[CAN_CREATE]:
             raise SecurityException(f'Creating {model_name} records is not allowed')
 
     def on_read_one(self, repo_cls, model):
         model_name = self._get_model_name_from_repo_cls(repo_cls)
+        if '*' in self.user_role[CANT_READ]:
+            raise SecurityException(f'Cannot read this {model_name}! '
+                                    f'Insufficient permission.')
+
         if model_name in self.user_role[CANT_READ]:
             for field in self.user_role[CANT_READ][model_name]:
+                if field == '*':
+                    raise SecurityException(f'Cannot read this {model_name}! '
+                                            f'Insufficient permission.')
+
                 if hasattr(model, field):
                     delattr(model, field)
 
     def on_read_many(self, repo_cls, models):
         raise NotImplementedError('lib.repository._call_middlewares currently '
-                                  'calls on_read_one for every model read')
+                                  'calls on_read_one iteratively')
 
-    def on_update(self, repo_cls, updated_model, id_attr='id'):
+    def on_update(self, repo_cls, updated_model, id_attr='id'):  # pylint: disable=too-many-branches
+        """
+        Uses dictdiffer to find the differences in the dictionary versions of the
+        old and new model.
+
+        If the following dictionaries were given:
+
+        a_dict = {
+            'a': 'foo',
+            'b': 'bar',
+            'd': 'barfoo'
+        }
+
+        b_dict = {
+            'a': 'foo',
+            'b': 'BAR',
+            'c': 'foobar'
+        }
+
+        The dictdiffer tool would return the following:
+
+        difference = list(dictdiffer.diff(a_dict, b_dict))
+        print(difference)
+            [
+                ('change', 'b', ('bar', 'BAR')),
+                ('add', '', [('c', 'foobar')]),
+                ('remove', '', [('d', 'barfoo')])
+            ]
+        """
+        model_name = self._get_model_name_from_repo_cls(repo_cls)
+
+        if CAN_UPDATE not in self.user_role:
+            raise SecurityException(f'Updating {model_name} records is not allowed')
+
+        if '*' in self.user_role[CAN_UPDATE]:
+            return
+
+        if model_name not in self.user_role[CAN_UPDATE]:
+            raise SecurityException(f'Updating {model_name} records is not allowed')
+
+        # self.user_role[CAN_UPDATE][model_name] exists
+        # now to look at the nitty-gritty of the changes...
+
         old_model = repo_cls.read(getattr(updated_model, id_attr), id_attr=id_attr)
 
-        # TODO do difference, check if it follows role policy
+        for action, field, values in \
+                list(dictdiffer.diff(old_model.to_dict(), updated_model.to_dict())):
+            if action == 'add':
+                print('Warning: attempt to add a field made. Was this desired?')
+                for couple in values:
+                    for key, _ in couple:
+                        if key not in self.user_role[CAN_UPDATE][model_name]:
+                            raise SecurityException(f'Updating the {key} field in '
+                                                    f'{model_name} records is not allowed')
+            elif action == 'remove':
+                for couple in values:
+                    for key, _ in couple:
+                        if key not in self.user_role[CAN_UPDATE][model_name]:
+                            raise SecurityException(f'Updating the {key} field in '
+                                                    f'{model_name} records is not allowed')
+            elif action == 'change':
+                if field not in self.user_role[CAN_UPDATE][model_name]:
+                    raise SecurityException(f'Updating the {field} field in '
+                                            f'{model_name} records is not allowed')
 
     def on_destroy(self, repo_cls, model_id, id_attr='id'):
         model_name = self._get_model_name_from_repo_cls(repo_cls)
-        if model_name not in self.user_role['can_destory']:
+
+        if CAN_DESTROY not in self.user_role:
+            raise SecurityException(f'Destroying {model_name} records is not allowed')
+
+        if '*' in self.user_role[CAN_DESTROY]:
+            return
+
+        if model_name not in self.user_role[CAN_DESTROY]:
             raise SecurityException(f'Destroying {model_name} records is not allowed')
